897ecf2af7058b6ea510eb58a5f08e5e5b975c57
Jay Kickliter <jay@kickliter.com>
2025-05-08 13:10:23 -0600

    add i16/f32 temp conversions

--- src/types.rs
+++ src/types.rs
@@ -1,7 +1,7 @@
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub struct Temperature(pub f32);
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct RawTemperature {
     pub msb: u8,
     pub lsb: u8,
@@ -10,6 +10,34 @@ pub struct RawTemperature {
 impl From<RawTemperature> for Temperature {
     fn from(other: RawTemperature) -> Self {
         Temperature::from_raw(other)
+    }
+}
+
+impl From<RawTemperature> for i16 {
+    fn from(other: RawTemperature) -> i16 {
+        other.to_i16()
+    }
+}
+
+impl From<RawTemperature> for f32 {
+    fn from(other: RawTemperature) -> f32 {
+        other.to_f32()
+    }
+}
+
+impl RawTemperature {
+    fn to_f32(self) -> f32 {
+        match (self.msb as i8).is_negative() {
+            true => (self.msb as f32 * 16.0 + self.lsb as f32 / 16.0) - 4096.0,
+            false => self.msb as f32 * 16.0 + self.lsb as f32 / 16.0,
+        }
+    }
+
+    fn to_i16(self) -> i16 {
+        match (self.msb as i8).is_negative() {
+            true => (self.msb as i16 * 16 + self.lsb as i16 / 16) - 4096,
+            false => self.msb as i16 * 16 + self.lsb as i16 / 16,
+        }
     }
 }
 
@@ -23,8 +51,8 @@ impl Temperature {
 #[inline]
 fn convert_temperature(buffer: RawTemperature) -> f32 {
     match (buffer.msb as i8).is_negative() {
-        false => buffer.msb as f32 * 16.0 + buffer.lsb as f32 / 16.0,
         true => (buffer.msb as f32 * 16.0 + buffer.lsb as f32 / 16.0) - 4096.0,
+        false => buffer.msb as f32 * 16.0 + buffer.lsb as f32 / 16.0,
     }
 }
 
@@ -39,25 +67,27 @@ mod tests {
             msb: 0b0000_1100u8,
             lsb: 0b0101_0010u8,
         };
-        let temperature = convert_temperature(data);
-        assert_eq!(temperature, 197.125);
+        assert_eq!(data.to_i16(), 197);
+        assert_eq!(data.to_f32(), 197.125);
     }
+
     #[test]
     fn test_negative_convert_temperature() {
         let data = RawTemperature {
             msb: 0b1111_0011u8,
             lsb: 0b1010_1101u8,
         };
-        let temperature = convert_temperature(data);
-        assert_eq!(temperature, -197.1875);
+        assert_eq!(data.to_i16(), -198);
+        assert_eq!(data.to_f32(), -197.1875);
     }
+
     #[test]
     fn test_from_raw() {
         let raw = RawTemperature {
             msb: 0b1000_1011u8,
             lsb: 0b1010_1110u8,
         };
-        let temperature = Temperature::from(raw);
-        assert_eq!(temperature, Temperature(-1861.125))
+        assert_eq!(i16::from(raw), -1862);
+        assert_eq!(f32::from(raw), -1861.125);
     }
 }
